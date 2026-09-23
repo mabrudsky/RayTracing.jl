13a10f64558559217310ad4f861bda3a91a99a96
Documenter.jl <documenter@juliadocs.github.io>
2026-09-23 09:36:58 +0000

    build based on 22655e1

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"category":"section","location":"#RayTracing","page":"Home","text":"Documentation for RayTracing.\n\n","title":"RayTracing"}]
+[{"category":"section","location":"#RayTracing","page":"Home","text":"Documentation for RayTracing.\n\n","title":"RayTracing"},{"category":"function","location":"#RayTracing.christoffel","page":"Home","text":"Declare \"christoffel\" here first, with no implementation yet,     so that every spacetime module (Kerr, Schwarzschild, etc.) adds     to this SAME function instead of each one creating its own.\n\n\n\n\n\n","title":"RayTracing.christoffel"}]
 }
