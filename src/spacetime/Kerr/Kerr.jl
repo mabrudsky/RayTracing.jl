b21097b187a601f1f7a59e877a7bb373fa17c4c7
martin-abrudsky <mabrudsky07@gmail.com>
2026-09-26 19:31:51 +0200

    Actualizar importación de _christoffel en el módulo kerr para utilizar _christoffel_ForwardDiff

--- src/spacetime/Kerr/Kerr.jl
+++ src/spacetime/Kerr/Kerr.jl
@@ -2,7 +2,7 @@ module kerr
 using StaticArrays
 using Parameters
 using ..RayTracing: AbstractSpacetime, AbstractKerrSpacetime, AbstractCoordinate, CartesianCoordinate, SphericalCoordinate
-using ..RayTracing: AbstractChristoffel, ChristoffelTag, _Γcomp, _christoffel
+using ..RayTracing: AbstractChristoffel, ChristoffelTag, _Γcomp, _christoffel_ForwardDiff
 
 import ..RayTracing: metric, metric_inverse, christoffel # Bring in "metric", "metric_inverse" and "christoffel" so this module adds a new case to each instead of accidentally creating a separate, disconnected function.
 #------------------------------- spacetime --------------------------------- 
